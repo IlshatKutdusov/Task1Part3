ab6c4d3e036a30b0bb86aa64cfb80ad6e29cb276
IlshatKutdusov <vip.sanki007@bk.ru>
2021-03-09 21:38:06 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppCSharp/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using ClassLibraryFSharp;
+
+namespace ConsoleAppCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Инициализация Random'а
+                Random MyRandom = new Random();
+
+                //Количество элементов в массиве
+                int n = MyRandom.Next(1, 21);
+
+                //Исходный массив
+                int[] MyArray = new int[n];
+
+                //Заполнение массива
+                for (int i = 0; i < n; i++)
+                    MyArray[i] = MyRandom.Next(-20, 21);
+
+                //Вывод состояния массива до сортировки
+                Console.WriteLine("Source array:");
+                foreach (int x in MyArray)
+                {
+                    Console.Write(" " + x);
+                }
+
+                //Вызов метода QuickSort из модуля MyModule в ClassLibraryFSharp
+                MyArray = MyModule.QuickSort(MyArray);
+
+                //Вывод состояния массива после сортировки
+                Console.WriteLine("\nSorted array:");
+                foreach (int x in MyArray)
+                {
+                    Console.Write(" " + x);
+                }
+            }
+            catch (Exception ex)
+            {
+                //Вывод сообщения об ошибке
+                Console.WriteLine("Error! Message: " + ex);
+            }
+
+            //Консолька в режиме ("ожидания")
+            Console.ReadKey();
+        }
+    }
+}
